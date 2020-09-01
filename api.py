b232c46244a1c3ae03fd0239fdb065702360eca5
Gerald <geraldjoshua7@gmail.com>
2020-09-01 23:24:48 +0200

    added an automated call timer for the internet measurements

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
 import time
 import pandas as pd
-
+import SpeedcheckerMeasure as sc
+import CaidaMeasure as cm
+import RipeMeasure as rm
+from flask import jsonify, make_response
 
 from flask import Flask, render_template, request
 
@@ -9,5 +12,43 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    #return render_template("/index.html",error=False)
+    return render_template("/index.html", error=False)
+
+
+@app.route("/simulate")
+def simulate():
     return render_template("/simulate.html", error=False)
+
+
+@app.route("/create-measurement", methods=["POST"])
+def create_measurement():
+    req = request.get_json()
+    platform = str(req['platforms']).strip()
+    if platform == "SpeedChecker":
+        sc.get_available_probes()
+        time.sleep(60)
+        sc.post_trace_all_ip_test()
+    elif platform == "RIPE":
+        rm.post_trace_all_ip_test()
+    elif platform == "CAIDA":
+        cm.post_trace_all_ip_test()
+
+    res = make_response(jsonify({"message": "OK"}), 200)
+
+    return res
+
+
+@app.route("/fetch-measurement", methods=["POST"])
+def fetch_measurement():
+    req = request.get_json()
+    platform = str(req['platforms']).strip()
+    if platform == "SpeedChecker":
+        sc.get_trace_all_result()
+    elif platform == "RIPE":
+        rm.get_trace_all_result()
+    elif platform == "CAIDA":
+        cm.get_trace_all_result()
+
+    res = make_response(jsonify({"message": "OK"}), 200)
+
+    return res
