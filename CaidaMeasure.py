9d3f64d138fea5b5a920567dcaff1925ab845eb9
Gerald <geraldjoshua7@gmail.com>
2020-09-16 13:41:45 +0200

    incorporated mongodb-online

--- CaidaMeasure.py
+++ CaidaMeasure.py
@@ -1,8 +1,9 @@
-import requests
 import json
 import random
-import os
-import shutil
+
+import requests
+
+import MongoOperations as mo
 
 """
 get all active probes in Africa
@@ -10,6 +11,8 @@ get all active probes in Africa
 api_key = ""
 g_base_url = "https://vela.caida.org/api"
 g_timeout = 120  # default timeout
+ping_test_id = []
+trace_test_id = []
 
 
 def get_available_probes():
@@ -34,13 +37,14 @@ def get_available_probes():
     return africa_probes
 
 
-def post_trace_all_ip_test():
+def post_trace_all_ip_test(ip_Africa_Address):
+    global trace_test_id
     africa_probes = get_available_probes()
     params = {'key': api_key, "method": "traceroute"}
-    file = open("files/ip_Africa_address.txt", 'r')
-    ip_address = file.readlines()
+    #file = open("files/ip_Africa_address.txt", 'r')
+    ip_address = ip_Africa_Address #file.readlines()
     ip_address = random.sample(ip_address, len(ip_address))
-    file.close()
+    #file.close()
     ip_start = 0
     result_id = []
     for probe in africa_probes:
@@ -59,22 +63,24 @@ def post_trace_all_ip_test():
             if result["result"] != "error":
                 result_id.append(result["result_id"])
 
-    if os.path.exists("files/trace_test_id.txt"):
-        os.remove("files/trace_test_id.txt")
-    file = open("files/trace_test_id.txt", "a")
-    for tid in result_id:
-        file.write(str(tid))
-        file.write('\n')
-    file.close()
+    trace_test_id = result_id
+    # if os.path.exists("files/trace_test_id.txt"):
+    #     os.remove("files/trace_test_id.txt")
+    # file = open("files/trace_test_id.txt", "a")
+    # for tid in result_id:
+    #     file.write(str(tid))
+    #     file.write('\n')
+    # file.close()
 
 
-def post_ping_all_ip_test():
+def post_ping_all_ip_test(ip_Africa_Address):
+    global ping_test_id
     africa_probes = get_available_probes()
     params = {'key': api_key, "method": "ping"}
-    file = open("files/ip_Africa_address.txt", 'r')
-    ip_address = file.readlines()
+    #file = open("files/ip_Africa_address.txt", 'r')
+    ip_address = ip_Africa_Address #file.readlines()
     ip_address = random.sample(ip_address, len(ip_address))
-    file.close()
+    #file.close()
     ip_start = 0
     result_id = []
     for probe in africa_probes:
@@ -93,63 +99,73 @@ def post_ping_all_ip_test():
             if result["result"] != "error":
                 result_id.append(result["result_id"])
 
-    if os.path.exists("files/ping_test_id.txt"):
-        os.remove("files/ping_test_id.txt")
-    file = open("files/ping_test_id.txt", "a")
-    for tid in result_id:
-        file.write(str(tid))
-        file.write('\n')
-    file.close()
+    ping_test_id = result_id
+    # if os.path.exists("files/ping_test_id.txt"):
+    #     os.remove("files/ping_test_id.txt")
+    # file = open("files/ping_test_id.txt", "a")
+    # for tid in result_id:
+    #     file.write(str(tid))
+    #     file.write('\n')
+    # file.close()
 
 
 def get_trace_all_result():
-    if os.path.exists("files/trace"):
-        shutil.rmtree("files/trace")
-
+    # if os.path.exists("files/trace"):
+    #     shutil.rmtree("files/trace")
+    #
     params = {'key': api_key}
-    os.mkdir("files/trace")
-    file = open("files/trace_test_id.txt")
-    result_id = file.readlines()
-    for id in result_id:
-        params["id"] = id.strip()
-        try:
-            r = requests.get(g_base_url + "/results", params=params, timeout=g_timeout)
-        except requests.exceptions.RequestException as e:
-            return "Request GET RESULT FAILED"
-        result = r.json()
-        if result["result"] != "error" and result["status"] == "completed":
-            for k in ["values"]:
-                for mon, v in result[k].items():
-                    s = "files/trace/trace" + id.strip() + ".txt"
-                    with open(s, 'w') as outfile:
-                        json.dump(json.JSONDecoder().decode(v), outfile)
+    # os.mkdir("files/trace")
+    # file = open("files/trace_test_id.txt")
+    # result_id = file.readlines()
+    for id in trace_test_id:
+        if id is not None:
+            params["id"] = id.strip()
+            try:
+                r = requests.get(g_base_url + "/results", params=params, timeout=g_timeout)
+            except requests.exceptions.RequestException as e:
+                return "Request GET RESULT FAILED"
+            result = r.json()
+            if result["result"] != "error" and result["status"] == "completed":
+                for k in ["values"]:
+                    for mon, v in result[k].items():
+                        res = json.JSONDecoder().decode(v)
+                        mo.upload_to_mongo("CAIDA", res)
+                        # s = "files/trace/trace" + id.strip() + ".txt"
+                        # with open(s, 'w') as outfile:
+                        #     json.dump(json.JSONDecoder().decode(v), outfile)
 
 
 def get_ping_all_result():
-    if os.path.exists("files/ping"):
-        shutil.rmtree("files/ping")
+    # if os.path.exists("files/ping"):
+    #     shutil.rmtree("files/ping")
     params = {'key': api_key}
-    file = open("files/ping_test_id.txt")
-    os.mkdir("files/ping")
-    result_id = file.readlines()
-    for id in result_id:
-        params["id"] = id
-        try:
-            r = requests.get(g_base_url + "/results", params=params, timeout=g_timeout)
-        except requests.exceptions.RequestException as e:
-            return "Request GET RESULT FAILED"
-        result = r.json()
-        if result["result"] != "error" and result["status"] == "completed":
-            for k in ["values"]:
-                for mon, v in result[k].items():
-                    s = "files/ping/ping" + id.strip() + ".txt"
-                    with open(s, 'w') as outfile:
-                        json.dump(json.JSONDecoder().decode(v), outfile)
+    # file = open("files/ping_test_id.txt")
+    # os.mkdir("files/ping")
+    # result_id = file.readlines()
+    for id in ping_test_id:
+        if id is not None:
+            params["id"] = id
+            try:
+                r = requests.get(g_base_url + "/results", params=params, timeout=g_timeout)
+            except requests.exceptions.RequestException as e:
+                return "Request GET RESULT FAILED"
+            result = r.json()
+            if result["result"] != "error" and result["status"] == "completed":
+                for k in ["values"]:
+                    for mon, v in result[k].items():
+                        res = json.JSONDecoder().decode(v)
+                        mo.upload_ping_to_mongo("CAIDA", res)
+                        # s = "files/ping/ping" + id.strip() + ".txt"
+                        # with open(s, 'w') as outfile:
+                        #     json.dump(json.JSONDecoder().decode(v), outfile)
 
 
 # def main():
-#     # post_trace_all_ip_test()
+#     post_trace_all_ip_test()
+#     post_ping_all_ip_test()
+#     time.sleep(2400)
 #     get_trace_all_result()
+#     get_ping_all_result()
 #
 #
 # if __name__ == "__main__":
--- IpFetcher.py
+++ IpFetcher.py
@@ -1,72 +1,85 @@
+import os
+
 from selenium import webdriver
 from ipaddress import IPv4Network
 from bs4 import BeautifulSoup
 from chromedriver_py import binary_path  # this will get you the path variable
-import os
 from selenium.webdriver.chrome.options import Options
 import random
-import subprocess
+import pyasn
 
-# set the path to configure webdriver to use chrome browser
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(executable_path=binary_path, chrome_options=chrome_options)
+Africa_asn = []
 
-file = open("files/countries.txt", "r")
-country = file.readlines()
 
-asn = []
-for code in country:
-    cod = code.lower().strip()
-    driver.get("https://ipinfo.io/countries/" + cod)
-    content = driver.page_source
-    soup = BeautifulSoup(content, features="html.parser")
-    driver.implicitly_wait(25)
-    for body in soup.findAll('tr'):
-        temp = []
-        for a in body.findAll('td', attrs={'class': 'p-3'}):
-            temp.append(a.text.strip())
-        driver.implicitly_wait(10)
-        if len(temp) != 0 and temp[2] != 0:
-            asn.append(temp[0])
+def scrape_africa_asn():
+    global Africa_asn
+    # set the path to configure webdriver to use chrome browser
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(executable_path=binary_path, chrome_options=chrome_options)
 
-if os.path.exists("files/AsnAfrica.txt"):
-    os.remove("files/AsnAfrica.txt")
-file = open("files/AsnAfrica.txt", "a")
+    country = ["DZ", "AO", "BJ", "BW", "BF", "BI", "CM", "CV", "CF", "TD", "KM", "CG", "CD", "CI", "DJ",
+               "EG", "GQ", "ER", "ET", "GA", "GM", "GH", "GN", "GW", "KE", "LS", "LR", "LY", "MG", "MW",
+               "ML", "MR", "MU", "YT", "MA", "MZ", "NA", "NE", "NG", "RE", "RW", "SH", "ST", "SN", "SC",
+               "SL", "SO", "ZA", "SS", "SD", "SZ", "TZ", "TG", "TN", "UG", "EH", "ZM", "ZW"]
+    asn = []
+    for code in country:
+        cod = code.lower().strip()
+        driver.get("https://ipinfo.io/countries/" + cod)
+        content = driver.page_source
+        soup = BeautifulSoup(content, features="html.parser")
+        driver.implicitly_wait(25)
+        for body in soup.findAll('tr'):
+            temp = []
+            for a in body.findAll('td', attrs={'class': 'p-3'}):
+                temp.append(a.text.strip())
+            driver.implicitly_wait(10)
+            if len(temp) != 0 and temp[2] != 0:
+                asn.append(temp[0])
+    Africa_asn = asn
+    driver.quit()
 
-for a in asn:
-    file.write(a.strip())
-    file.write('\n')
-file.close()
 
-# running bash script
-print("start")
-subprocess.call("bash ip.sh", shell=True)
-print("end")
+def get_random_africa_ip():
+    # Initialize module and load IP to ASN database
+    asndb = pyasn.pyasn('files/ipasn.dat')
+    netarr = []
 
-netarr = []
-f = open("ipnetwork.txt", "r")
-for p in f:
-    p = p.split(" ")
-    netarr.append(p[1].strip())
-count = 0
-randomip = []
-ipaddress = []
-for net in netarr:
-    net = IPv4Network(net)
-    for addr in net:
-        randomip.append(addr)
-    # randomize the array
-    randomip = random.sample(randomip, len(randomip))
-    ipaddress.append(randomip[0])
-    ipaddress.append(randomip[1])
-    randomip.clear()
+    for asn1 in Africa_asn:
+        iprange = asndb.get_as_prefixes(int(asn1[2:]))
+        if iprange is not None:
+            for i in iprange:
+                netarr.append(str(i).strip())
+                break
 
-if os.path.exists("files/ip_Africa_address.txt"):
-    os.remove("files/ip_Africa_address.txt")
-file = open("files/ip_Africa_address.txt", 'a')
-for ip in ipaddress:
-    file.write(str(ip))
-    file.write('\n')
-file.close()
-driver.quit()
+    randomip = []
+    ipaddress = []
+    for net in netarr:
+        net = IPv4Network(net)
+        for addr in net:
+            randomip.append(addr)
+        # randomize the array
+        randomip = random.sample(randomip, len(randomip))
+        ipaddress.append(randomip[0])
+        ipaddress.append(randomip[1])
+        randomip.clear()
+
+    #delete later
+    if os.path.exists("files/ip_Africa_address.txt"):
+        os.remove("files/ip_Africa_address.txt")
+    file = open("files/ip_Africa_address.txt", 'a')
+    for ip in ipaddress:
+        file.write(str(ip))
+        file.write('\n')
+    file.close()
+
+    return ipaddress
+
+
+def main():
+    scrape_africa_asn()
+    print(len(get_random_africa_ip()))
+
+
+if __name__ == "__main__":
+    main()
